b29b2edf024ef2dc498d6e1690bd7254a1802b5d
Adrian-Yunas <adrian.yunas@si.ukdw.ac.id>
2022-02-09 14:00:45 +0700

    Initial commit

---
+++ src/data/charities.js
@@ -0,0 +1,15 @@
+module.exports = { charities: [
+    {
+        id: 1,
+        title: 'First Charity Project',
+        category: 'Money',
+        thumbnail: 'https://live.staticflickr.com/65535/17123251389_80282733ce.jpg',
+        pledged: 99000,
+        target: 100000,
+        date_end: +new Date('15 August 2022'),
+        profile_photo: 'https://live.staticflickr.com/65535/51864452987_7f54fe6056_s.jpg',
+        profile_name: 'Adrian Yunas',
+        no_pledges: 0
+    }
+]
+ }
---
+++ src/pages/Home.svelte
@@ -0,0 +1,16 @@
+<script>
+  import CharityList from "../components/CharityList.svelte";
+  import Header from "../components/header.svelte";
+  import Welcome from "../components/welcome.svelte";
+  import Promo from "../components/promo.svelte";
+  import Footer from "../components/footer.svelte";
+  import { charities } from "../data/charities.js";
+
+  let title = "Charity";
+</script>
+
+<Header />
+<Welcome />
+<CharityList {charities} />
+<Promo />
+<Footer />
