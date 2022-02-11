03f10e90cd2a425818d091e4d94c05e81c52716c
Adrian-Yunas <adrian.yunas@si.ukdw.ac.id>
2022-02-11 13:30:14 +0700

    netlify fix

--- src/pages/Contact.svelte
+++ src/pages/Contact.svelte
@@ -1,5 +1,5 @@
 <script>
-  import Header from "../components/header.svelte";
+  import Header from "../components/Header.svelte";
   import Footer from "../components/Footer.svelte";
 
   google.maps.event.addDomListener(window, "load", init);
